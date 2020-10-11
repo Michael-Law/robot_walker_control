7bc44d0f2032655f206bf9604ee8a788eef1d3fe
Michael-Law <mlawsan@outlook.com>
2020-10-11 23:15:25 +0400

    Corrections for Indirect KInematics

--- src/indirect_kinematics.py
+++ src/indirect_kinematics.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from srv import *
-import rospy
 import sys
 import copy
 import rospy
@@ -11,44 +9,148 @@ import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
+## END_SUB_TUTORIAL
 
 
+def all_close(goal, actual, tolerance):
+  """
+  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+  @param: goal       A list of floats, a Pose or a PoseStamped
+  @param: actual     A list of floats, a Pose or a PoseStamped
+  @param: tolerance  A float
+  @returns: bool
+  """
+  all_equal = True
+  if type(goal) is list:
+    for index in range(len(goal)):
+      if abs(actual[index] - goal[index]) > tolerance:
+        return False
 
-class groupService: 
-	def __init__(self):
-		rospy.init_node('indirect_service_server'%(self.group_name))
-		self.group_name = rospy.get_param('~group_name', "Middle")
+  elif type(goal) is geometry_msgs.msg.PoseStamped:
+    return all_close(goal.pose, actual.pose, tolerance)
 
-		moveit_commander.roscpp_initialize(sys.argv)
-		rospy.init_node('%s_service_server'%(self.group_name), anonymous=True)
-		self.robot=moveit_commander.RobotCommander()
-		self.scene =moveit_commander.PlanningSceneInterface()
-		self.move_group =moveit_commander.MoveGroupCommander(self.group_name)
-		rospy.Service('indirect_kin_service_%s'%(self.group_name), indirect_kin_service, self.handle_req)
-		rospy.spin()
+  elif type(goal) is geometry_msgs.msg.Pose:
+    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+  return True
 
 
-	def handle_req(self,req):
-		try:
-			pose_goal = geometry_msgs.msg.Pose()
-			pose_goal.orientation.w = req.pose.orientation.w
-			pose_goal.position.x = req.pose.position.x
-			pose_goal.position.y = req.pose.position.y
-			pose_goal.position.z = req.pose.position.z
-			
-			self.move_group.set_pose_target(pose_goal)
-			self.move_group.go(wait=True)
-			self.move_group.stop()
-			self.move_group.clear_pose_targets()
+class MoveGroupPythonIntefaceTutorial(object):
+  """MoveGroupPythonIntefaceTutorial"""
+  def __init__(self):
+    super(MoveGroupPythonIntefaceTutorial, self).__init__()
 
-			rep=indirect_kin_serviceResponse()
-			rep.res.data=True
-			rep.message.data="Success"
-			return rep
-		except Exception as e:
-			rep=direct_kin_serviceResponse()
-			rep.message.data=e
-			return rep
+    ## BEGIN_SUB_TUTORIAL setup
+    ##
+    ## First initialize `moveit_commander`_ and a `rospy`_ node:
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
 
-if __name__ == "__main__":
-	groupService()
+    ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+    ## kinematic model and the robot's current joint states
+    robot = moveit_commander.RobotCommander()
+
+    ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+    ## for getting, setting, and updating the robot's internal understanding of the
+    ## surrounding world:
+    scene = moveit_commander.PlanningSceneInterface()
+
+	# Group_list = robot.get_group_names()
+    group_name = "primary_left_foot"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
+    ## trajectories in Rviz:
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+
+ 
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+    # We can get the name of the reference frame for this robot:
+    planning_frame = move_group.get_planning_frame()
+
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = move_group.get_end_effector_link()
+
+
+    # We can get a list of all the groups in the robot:
+    group_names = robot.get_group_names()
+
+
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+
+    print(robot.get_current_state())
+    print("")
+    ## END_SUB_TUTORIAL
+
+    # Misc variables
+    self.box_name = ''
+    self.robot = robot
+    self.scene = scene
+    self.move_group = move_group
+    self.display_trajectory_publisher = display_trajectory_publisher
+    self.planning_frame = planning_frame
+    self.eef_link = eef_link
+    self.group_names = group_names
+
+
+  def go_to_pose_goal(self):
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    move_group = self.move_group
+    tpose = move_group.get_current_pose()
+    print type(tpose)
+    print tpose
+    ##
+    ## Planning to a Pose Goal
+    ## ^^^^^^^^^^^^^^^^^^^^^^^
+    ## We can plan a motion for this group to a desired pose for the
+    ## end-effector:
+    pose_goal = geometry_msgs.msg.Pose()
+    # print(move_group)	
+
+    # pose_goal.orientation.w += 0.0
+    # pose_goal.orientation.y += 0.0
+    # pose_goal.orientation.x += 0.0		
+    # pose_goal.orientation.z += 0.0
+    pose_goal.position.y = 0.0
+    pose_goal.position.x = 0.0	
+    pose_goal.position.z = 0.0
+
+    
+    # move_group.go(tpose,wait = True)
+
+    plan = move_group.go(wait=True)
+    move_group.stop()
+    
+    move_group.clear_pose_targets()
+
+    ## END_SUB_TUTORIAL
+
+    # For testing:
+    # Note that since this section of code will not be included in the tutorials
+    # we use the class variable rather than the copied state variable
+    current_pose = self.move_group.get_current_pose().pose
+    return all_close(pose_goal, current_pose, 0.0001)
+
+
+  
+
+ 
+
+def main():
+  try:
+    tutorial = MoveGroupPythonIntefaceTutorial()
+    tutorial.go_to_pose_goal()
+
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return
+
+if __name__ == '__main__':
+  main()
